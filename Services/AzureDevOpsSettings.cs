e0409bd861bcab58217ac3be54424d0a7fb1ecc9
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2025-02-22 21:00:13 -0500

    Create AzureDevOpsSettings.cs

---
+++ Services/AzureDevOpsSettings.cs
@@ -0,0 +1,7 @@
+namespace SystemProvisioningPortal.Services;
+
+public class AzureDevOpsSettings
+{
+    public string ApiUrl { get; set; } = string.Empty;
+    public string PersonalAccessToken { get; set; } = string.Empty;
+}
